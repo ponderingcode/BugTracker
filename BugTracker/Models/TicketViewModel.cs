39ab20649cd73c29169129d64b3b440b00893b38
= <=>
2016-10-03 23:31:54 -0400

    fixed dropdown lists for ticket status, type, & priority in ticket edit view

---
+++ BugTracker/Models/TicketViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace BugTracker.Models
+{
+    public class TicketViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public SelectList Priority { get; set; }
+        public SelectList Type { get; set; }
+        public SelectList Status { get; set; }
+        public string SelectedPriority { get; set; }
+        public string SelectedType { get; set; }
+        public string SelectedStatus { get; set; }
+    }
+}
